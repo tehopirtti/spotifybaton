466c92a62a7df0483a018fe19824116619927bdb
lmnoop <raina@kapsi.fi>
2025-04-30 16:12:51 +0300

    Improve(?) cover art dominant color determination
    and fix image source loading before crossOrigin = "anonymous" is set
    resulting in canvas getImageData() being considered an insecure DOM
    operation

--- view/blaster/functions.js
+++ view/blaster/functions.js
@@ -1,15 +1,27 @@
 const canvas = document.createElement("canvas");
-canvas.height = 1;
-canvas.width = 1;
+canvas.height = 16;
+canvas.width = 16;
+
+const img = new Image();
 
 function colorize() {
-	canvas.getContext("2d").drawImage(img, 0, 0, 1, 1);
-	color = canvas.getContext("2d").getImageData(0, 0, 1, 1);
-	document.documentElement.style.setProperty("--accent-color", `rgb(${color.data[0]}, ${color.data[1]}, ${color.data[2]})`);
+	canvas.getContext("2d").drawImage(img, 0, 0, canvas.width, canvas.height);
+	thumb = canvas.getContext("2d").getImageData(0, 0, canvas.width, canvas.width);
+	let r = 0, g = 0, b = 0;
+	for (let i = 0; i < thumb.data.length; i++) {
+		r += thumb.data[i++];
+		g += thumb.data[i++];
+		b += thumb.data[i++];
+	}
+	r = (r / (thumb.data.length * .25));
+	g = (g / (thumb.data.length * .25));
+	b = (b / (thumb.data.length * .25));
+	r = ~~(r < 128 ? r = (r + 128) / 2 : r);
+	g = ~~(g < 128 ? g = (g + 128) / 2 : g);
+	b = ~~(b < 128 ? b = (b + 128) / 2 : b);
+	document.documentElement.style.setProperty("--accent-color", `rgb(${r}, ${g}, ${b})`);
 }
 
-img = new Image();
 img.addEventListener("load", colorize);
-img.src = document.querySelector(`#current img`).src;
 img.crossOrigin = "anonymous";
-colorize();
+img.src = document.querySelector(`#current img`).src;
